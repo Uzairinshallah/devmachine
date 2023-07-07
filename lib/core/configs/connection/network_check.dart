8896b45635f0025db17c4d1dd042324ab2d34be6
uzair inshallah <uzairinshallah1@gmail.com>
2023-07-07 14:58:14 +0500

    Initial commit

---
+++ lib/core/configs/connection/network_check.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mysite/app/sections/main/main_section.dart';
+import 'bloc/connected_bloc.dart';
+
+class NChecking extends StatelessWidget {
+  const NChecking({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<ConnectedBloc, ConnectedState>(
+      listener: (context, state) {
+      },
+      builder: (context, state) {
+        if (state is ConnectedSucessState) {
+          return const MainPage();
+        }
+        else {
+          return const MainPage();
+        }
+      },
+    );
+  }
+}
+
+class NoConnectionErorr extends StatelessWidget {
+  const NoConnectionErorr({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Connection failed!"),
+          ],
+        ),
+      ),
+    );
+  }
+}
+// TODO
